1ce706db4dde1ef7d7746ab4dcf787d073843d25
Mederic <kottomouyema.m@gmail.com>
2024-05-21 11:57:57 +0200

    Quete ASP.Net MVC

---
+++ Data/ASPNET_CORE_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASP.NET_CORE_MVC.Models;
+
+namespace ASP.NET_CORE_MVC.Data
+{
+    public class ASPNET_CORE_MVCContext : DbContext
+    {
+        public ASPNET_CORE_MVCContext (DbContextOptions<ASPNET_CORE_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP.NET_CORE_MVC.Models.Article> Article { get; set; } = default!;
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_CORE_MVC.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Contenu { get; set; } 
+
+    }
+}
